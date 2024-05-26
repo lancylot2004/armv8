c7bb1081317a7d7293a766ee44060e36d66e3606
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-26 18:34:19 +0100

    Adds function prototypes for `memory`.

---
+++ src/emulator/system/memory.h
@@ -0,0 +1,27 @@
+///
+/// memory.h
+/// Structure, manipulation, allocation, and freeing of the virtual memory.
+///
+/// Created by Lancelot Liu on 26/05/2024.
+///
+
+#ifndef EMULATOR_MEMORY_H
+#define EMULATOR_MEMORY_H
+
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+void *allocMemFromFile(size_t size, int fd);
+
+void *allocMem(size_t size);
+
+uint32_t readMem32(void *mem, size_t addr);
+
+uint64_t readMem64(void *mem, size_t addr);
+
+void writeMem32(void *mem, size_t addr, uint32_t value);
+
+void writeMem64(void *mem, size_t addr, uint64_t value);
+
+#endif //EMULATOR_MEMORY_H
