591d908a972bc155dd214743c9d32b0984eb26b2
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-27 12:38:24 +0100

    Creates outline of work for IO.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+    // TODO: Check input arguments are valid.
+
+    // TODO: File handler
+
+    // TODO: Instantiate registers and memory from input file
+
+    // TODO: Execution cycle, with halt
+
+    // TODO: Print stuff at the end.
+
+    return EXIT_SUCCESS;
 }
