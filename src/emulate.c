43ca29d90242c01bd2f74649ce8a1be6cb9989cd
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-27 13:03:57 +0100

    Adds `output` and implements `dumpRegs`.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

--- src/emulate.c
+++ src/emulate.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
 
+#include "emulator/system/registers.h"
+#include "emulator/io/output.h"
+
 int main(int argc, char **argv) {
     // TODO: Check input arguments are valid.
 
     // TODO: File handler
 
-    // TODO: Instantiate registers and memory from input file
+    Registers reg = createReg();
+
+    // TODO: Instantiate memory from input file
 
     // TODO: Execution cycle, with halt
 
     // TODO: Print stuff at the end.
+    dumpRegs(&reg);
 
     return EXIT_SUCCESS;
 }
