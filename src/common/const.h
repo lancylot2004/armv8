718eefd0c3b7a01963691ff300872664250ed5e6
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-28 09:42:03 +0100

    Corrected header docs for `const`.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

---
+++ src/common/const.h
@@ -0,0 +1,13 @@
+///
+/// const.h
+/// Constants for the rest of the program.
+///
+/// Created by Lancelot Liu on 26/05/2024.
+///
+
+#ifndef CONST_H
+#define CONST_H
+
+#define MEMORY_SIZE (2 << 20) // 2MB
+
+#endif //CONST_H
