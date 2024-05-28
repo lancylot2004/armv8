0835252997507d070951ac04d15460f2084ffc71
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-28 09:41:16 +0100

    Adds file for helper functions and string trimming.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

---
+++ src/common/helpers.h
@@ -0,0 +1,16 @@
+///
+/// helpers.h
+/// Utility functions for the rest of the program.
+///
+/// Created by Lancelot Liu on 28/05/2024.
+///
+
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <ctype.h>
+#include <string.h>
+
+void trim(char *str);
+
+#endif //HELPERS_H
