0544727b2446f0c5bd60bcb3fce4bbe467457c12
Jack Wong <jack.wong23@imperial.ac.uk>
2024-05-29 14:26:50 +0100

    Fetch - Execute cycle loop writen in, Decode/Execute is commented out until done.

---
+++ src/emulate.h
@@ -0,0 +1,20 @@
+///
+/// emulate.h
+/// Emulate ARM64 code
+///
+/// Created by Jack Wong on 29/05/2024.
+///
+
+#ifndef EMULATE_H
+#define EMULATE_H
+
+#include <stdlib.h>
+
+#include "emulator/system/registers.h"
+#include "emulator/system/memory.h"
+#include "emulator/io/output.h"
+// #include "instructions/decode.h"
+
+#define HALT_INSTR_C 0x8a000000
+
+#endif //EMULATE_H
