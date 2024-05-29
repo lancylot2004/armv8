08cabdc19ca0f7ca22742bb18c10f5e96f8832d0
Alexander Biraben-Renard <alexander.biraben-renard23@imperial.ac.uk>
2024-05-29 18:10:57 +0100

    Split processing into decode and execute using IR
    Pair-programmed with bh1123

--- src/emulator/instructions/decode.c
+++ src/emulator/instructions/decode.c
@@ -1,28 +1,38 @@
 ///
 /// decode.c
-/// Delegate the work to the correct instruction handler
+/// Decode an instruction word to its intermediate representation (IR)
 ///
 /// Created by Billy Highley on 27/05/2024.
 ///
 
 #include "decode.h"
 
-/// Delegates decoding and execution to the corresponding process function.
-/// @param word The instruction to process.
-void decode(Instruction word, Registers regs, Memory mem) {
+/// Decode an instruction word to its intermediate representation (IR)
+/// @param word The instruction to decode.
+/// @returns The IR of the instruction.
+IR decode(Instruction word) {
+
+    IR ir;
+
     Component op0 = decompose(word, OP0_M);
 
-    if      ((op0 & OP0_IMM_M) == OP0_IMM_C)
-            processImmediate(word, regs);
+    if ((op0 & OP0_IMM_M) == OP0_IMM_C) {
+        ir = decodeImmediate(word);
+    }
 
-    else if ((op0 & OP0_REG_M) == OP0_REG_C)
-            processRegister(word, regs);
+    else if ((op0 & OP0_REG_M) == OP0_REG_C) {
+        ir = decodeRegister(word);
+    }
 
-    else if ((op0 & OP0_LS_M)  == OP0_LS_C)
-            processLoadStore(word, regs, mem);
+    else if ((op0 & OP0_LS_M)  == OP0_LS_C) {
+        ir = decodeLoadStore(word);
+    }
 
-    else if ((op0 & OP0_BR_M)  == OP0_BR_C)
-            processBranch(word, regs, mem);
+    else if ((op0 & OP0_BR_M)  == OP0_BR_C) {
+        ir = decodeBranch(word);
+    }
 
-    else    printf("Unknown op0: %d", op0);
+    else printf("Unknown op0: %d", op0);
+
+    return ir;
 }
