866bdea7f0138594ce30f8fb115b495179d18802
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-29 15:58:13 +0100

    Removed incomplete files for merge.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

--- src/assembler/insts/sdtAssem.c
+++
@@ -1,8 +0,0 @@
-///
-/// sdtAssem.c
-/// Functions to parse from assembly and write as binary a Single Data Transfer instruction.
-///
-/// Created by Lancelot Liu on 29/05/2024.
-///
-
-#include "sdtAssem.h"
