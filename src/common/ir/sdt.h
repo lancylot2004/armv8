652bfa81867d6402e970bf1647fc73c892d1dbed
Billy Highley <billy.highley23@imperial.ac.uk>
2024-05-29 16:33:59 +0100

    Reformats sdt.h
    Pair-programmed with ab1223

--- src/common/ir/sdt.h
+++ src/common/ir/sdt.h
@@ -11,67 +11,84 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/// The intermediate representation of a single data transfer instruction.
 typedef struct {
+
     /// [1b] The bit-width of all the registers in the instruction: 0 for 32-bit, 1 for 64-bit.
     bool sf;
 
-    /// The type of the single data transfer instruction group instruction
+    /// The type of the single data transfer instruction group instruction.
     enum {
-        SDT, /// The single data transfer instruction type in the single data transfer instruction group
-        LL   /// The load literal instruction type in the single data transfer instruction group
+
+        SDT, ///< The single data transfer instruction type.
+        LL   ///< The load literal instruction type.
+
     } sdtType;
 
     /// [19b] The parameters for the single data transfer instruction group.
     union {
 
-        /// [19b] The parameters for the single data transfer instruction type (not load literal).
+        /// [19b] Single data transfer interpretation: constants struct.
         struct {
 
-            /// [1b] Unsigned offset flag. When this bit is set, the addressing mode is an Unsigned Offset.
+            /// [1b] Determines whether addressing mode is unsigned offset (1 = unsigned offset).
             bool u;
 
-            /// [1b] The type of data transfer. When 1, this is a load operation. Otherwise it is a store.
+            /// [1b] Determines the type of data transfer (0 = store, 1 = load).
             bool l;
 
-            /// The addressing mode
+            /// The addressing mode.
             enum {
-                REGISTER_OFFSET, /// Transfer address = Xn + uoffset
-                PRE_INDEXED,     /// Transfer address = Xn + simm9, Write-back = Xn + simm9
-                POST_INDEXED,    /// Transfer address = Xn, Write-back = Xn + simm9
-                UNSIGNED_OFFSET  /// Transfer address = Xn + Xm
+
+                /// Transfer address: \code Xn + uoffset \endcode
+                REGISTER_OFFSET,
+
+                /// Transfer address: \code Xn + simm9 \endcode
+                /// Write-back: \code Xn + simm9 \endcode
+                PRE_INDEXED,
+
+                /// Transfer address: \code Xn \endcode
+                /// Write-back: \code Xn + simm9 \endcode
+                POST_INDEXED,
+
+                /// Transfer address: \code Xn + Xm \endcode
+                UNSIGNED_OFFSET
+
             } addressingMode;
 
-            /// [12b] The offset. The meaning of the offset depends on the addressing mode.
+            /// [12b] Interpretation of offset (depending on addressing mode).
             union {
 
-                /// [5b] The code for register Xm. Used for the register offset addressing mode.
+                /// [5b] The code for register Xm, used for the register offset addressing mode.
                 uint8_t xm;
 
                 /// [10b] The parameters for the pre/post indexed addressing mode.
                 struct {
-                    /// [9b] The signed value used for the pre/post-index addressing mode.
+
+                    /// [9b] The signed value used for the pre/post-indexed addressing mode.
                     int16_t simm9;
 
-                    /// [1b] Determines which of pre/post-indexing the addressing mode is using: 0 for post-indexed, 1 for pre-indexed.
+                    /// [1b] Determines whether to use pre/post-indexing (0 = post-indexed, 1 = pre-indexed).
                     bool i;
-                } prePostIndexParams;
 
-                /// [12b] The immediate constant for the unsigned offset addressing mode.
+                } prePostIndex;
+
+                /// [12b] 12-bit unsigned immediate constant for the unsigned offset addressing mode.
                 uint16_t uoffset;
 
             } offset;
 
-            /// [5b] The base register.
+            /// [5b] The encoding of the base register.
             uint8_t xn;
 
         } sdtType;
 
-        /// [19b] The signed immediate value for the load literal instruction type.
+        /// [19b] Load literal interpretation: signed immediate value.
         int32_t simm19;
 
     } sdtGroup;
 
-    /// [5b] The target register.
+    /// [5b] The encoding of the target register.
     uint8_t rt;
 
 } SDT_IR;
