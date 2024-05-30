0f97d7e9c5d6eca5c8236ef262b15d9922a7dbee
Alexander Biraben-Renard <alexander.biraben-renard23@imperial.ac.uk>
2024-05-30 18:14:22 +0100

    Add error-checking to decodeImmediate

--- src/emulator/instructions/immediate/immediateDecode.c
+++ src/emulator/instructions/immediate/immediateDecode.c
@@ -6,9 +6,12 @@
 ///
 
 #include "immediateDecode.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /// Decode a immediate group instruction to its IR
 /// @param word The instruction to decode
+/// @returns The IR of word
 IR decodeImmediate(Instruction word) {
 
     // Initialise the IR
@@ -28,6 +31,7 @@ IR decodeImmediate(Instruction word) {
         case ARITH: {
 
             // Define an arithmetic type IR using the components of the instruction
+            // All arith types are valid since there are 4 of them encoded by 2 bits
             immIR.opc.arithType       = decompose(word, IMM_OPC_M);
             immIR.operand.arith.sh    = decompose(word, IMM_SH_M);
             immIR.operand.arith.imm12 = decompose(word, IMM_12_M);
@@ -39,9 +43,25 @@ IR decodeImmediate(Instruction word) {
         // Wide move instruction
         case WIDE_MOVE: {
 
-            // Define an wide move type IR using the components of the instruction
-            immIR.opc.wideMoveType       = decompose(word, IMM_OPC_M);
-            immIR.operand.wideMove.hw    = decompose(word, IMM_HW_M);
+            // Define a wide move type IR using the components of the instruction
+
+            // All arith types except 0x1 are valid since there are 3 of them encoded by 2 bits
+            uint8_t wideMoveType = decompose(word, IMM_OPC_M);
+            if (wideMoveType == 0x1) {
+                // 0x1 is not a valid wide move type
+                fprintf(stderr, "Invalid wide move type");
+                exit(-1);
+            }
+
+            immIR.opc.wideMoveType = wideMoveType;
+
+            uint8_t hw = decompose(word, IMM_HW_M);
+            if (immIR.sf == 0 && hw > 1) {
+                fprintf(stderr, "hw can only be 0 or 1 when sf is 0");
+                exit(-1);
+            }
+            immIR.operand.wideMove.hw = hw;
+
             immIR.operand.wideMove.imm16 = decompose(word, IMM_16_M);
             break;
 
