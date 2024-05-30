34c4308523f4e56bf8a6e46cc8560d345064c703
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-31 00:02:38 +0100

    Ports over Alex's binary literal shorthand with underscore support.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

--- src/common/const.h
+++ src/common/const.h
@@ -22,4 +22,24 @@ typedef uint64_t BitData;
 /// Alias for an instruction as a binary word.
 typedef uint32_t BitInst;
 
+/// Shorthand for binary literals. Ignores '_'s.
+/// @example \code b(1010_0101) = 0xA5 \endcode
+/// @author Alexander Biraben-Renard
+/// @param x The binary as a string.
+/// @warning May not be optimised at compile - do not abuse!
+#define b(x) toBinary(#x)
+
+static inline uint64_t toBinary(const char *str) {
+    uint64_t result = 0;
+    while (*str) {
+        if (*str == '-') {
+            str++;
+            continue;
+        }
+        result <<= 1;
+        result += *str++ - '0';
+    }
+    return result;
+}
+
 #endif //COMMON_CONST_H
