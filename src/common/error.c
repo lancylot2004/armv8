c15f73381858568b7412720be324dddd75227e69
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-05-30 21:34:42 +0100

    Adds `error`.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

---
+++ src/common/error.c
@@ -0,0 +1,16 @@
+///
+/// error.c
+/// "For when you need to skidaddle."
+///
+/// Created by Lancelot Liu on 30/05/2024.
+///
+
+#include "error.h"
+
+void assertFatal(bool condition, char message[]) {
+    if (!condition) {
+        fprintf(stderr, "%s\n", message);
+        perror("Fatal Error");
+        exit(-1);
+    }
+}
---
+++ src/common/error.h
@@ -0,0 +1,17 @@
+///
+/// error.h
+/// "For when you need to skidaddle."
+///
+/// Created by Lancelot Liu on 30/05/2024.
+///
+
+#ifndef COMMON_ERROR_H
+#define COMMON_ERROR_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void assertFatal(bool condition, char message[]);
+
+#endif //COMMON_ERROR_H
