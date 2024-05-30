5e2a917a4b49e58dec11b9c0955e3d7fcddb6ad7
Billy Highley <billy.highley23@imperial.ac.uk>
2024-05-30 16:27:53 +0100

    Implement execute for immediate type instructions

---
+++ src/emulator/instructions/immediate/immediateExecute.h
@@ -0,0 +1,17 @@
+///
+/// immediateExecute.h
+/// Delegates execution of an immediate instruction to its specific type
+///
+/// Created by Billy Highley on 27/05/2024.
+///
+
+#ifndef IMMEDIATEEXECUTE_H
+#define IMMEDIATEEXECUTE_H
+
+#include "../../../common/ir/ir.h"
+#include "../../system/registers.h"
+#include "../instructions.h"
+#include "arithmeticExecute.h"
+#include "wideMoveExecute.h"
+
+#endif //IMMEDIATEEXECUTE_H
