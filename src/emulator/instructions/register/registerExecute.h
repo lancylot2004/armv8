bedfb55afa572193e528d66eaec6df3703e40529
Billy Highley <billy.highley23@imperial.ac.uk>
2024-05-31 15:44:10 +0100

    Implements execute logic for register instructions

---
+++ src/emulator/instructions/register/registerExecute.h
@@ -0,0 +1,18 @@
+///
+/// registerExecute.h
+/// Delegates execution of a register instruction to its specific type
+///
+/// Created by Billy Highley on 27/05/2024.
+///
+
+#ifndef REGISTEREXECUTE_H
+#define REGISTEREXECUTE_H
+
+#include "../../../common/ir/ir.h"
+#include "../../system/registers.h"
+#include "../instructions.h"
+#include "arithmeticRegExecute.h"
+#include "bitLogicExecute.h"
+#include "multiplyExecute.h"
+
+#endif //REGISTEREXECUTE_H
