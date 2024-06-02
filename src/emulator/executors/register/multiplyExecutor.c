b717199f507cbd508f3400135c3016a73bc9b7d8
Billy Highley <billy.highley23@imperial.ac.uk>
2024-06-02 15:35:20 +0100

    Refactor multiplyExecutor.c

--- src/emulator/executors/register/multiplyExecutor.c
+++ src/emulator/executors/register/multiplyExecutor.c
@@ -11,20 +11,18 @@
 /// @param regIR IR for a register (multiply) instruction
 /// @param regs Pointer to registers
 void multiplyExecute(Register_IR *registerIR, Registers regs) {
+
     // Set ra value to the 64-bit or 32-bit value of the ra-encoded register, determined by sf
-    uint64_t ra = registerIR->sf ?
-                  getReg(regs, registerIR->operand.multiply.ra) :
-                  (uint32_t) getReg(regs, registerIR->operand.multiply.ra);
+    uint64_t ra = getReg(regs, registerIR->operand.multiply.ra);
+    ra = registerIR->sf ? ra : (uint32_t) ra;
 
     // Set rn value to the 64-bit or 32-bit value of the rn-encoded register, determined by sf
-    uint64_t rn = registerIR->sf ?
-                  getReg(regs, registerIR->rn) :
-                  (uint32_t) getReg(regs, registerIR->rn);
+    uint64_t rn = getReg(regs, registerIR->rn);
+    rn = registerIR->sf ? rn : (uint32_t) rn;
 
     // Set rm value to the 64-bit or 32-bit value of the rm-encoded register, determined by sf
-    uint64_t rm = registerIR->sf ?
-                  getReg(regs, registerIR->rm) :
-                  (uint32_t) getReg(regs, registerIR->rm);
+    uint64_t rm = getReg(regs, registerIR->rm);
+    rm = registerIR->sf ? rm : (uint32_t) rm;
 
     // Initialise result value
     uint64_t res;
