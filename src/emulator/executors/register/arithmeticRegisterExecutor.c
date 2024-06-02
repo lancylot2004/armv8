97c58708a2d9a5b1edfa6eda35e6281441c89741
Billy Highley <billy.highley23@imperial.ac.uk>
2024-06-02 15:32:49 +0100

    Refactor arithmetic / logic executors

--- src/emulator/executors/register/arithmeticRegisterExecutor.c
+++ src/emulator/executors/register/arithmeticRegisterExecutor.c
@@ -7,13 +7,10 @@
 
 #include "arithmeticRegisterExecutor.h"
 
-static bool overflow64(int64_t src, int64_t op2, int64_t res);
-
-static bool overflow32(int32_t src, int32_t op2, int32_t res);
-
-static bool underflow64(int64_t src, int64_t op2, int64_t res);
-
-static bool underflow32(int32_t src, int32_t op2, int32_t res);
+static bool overflow64(int64_t rn, int64_t op2, int64_t res);
+static bool overflow32(int32_t rn, int32_t op2, int32_t res);
+static bool underflow64(int64_t rn, int64_t op2, int64_t res);
+static bool underflow32(int32_t rn, int32_t op2, int32_t res);
 
 /// Execute an arithmetic type instruction
 /// @param regIR IR for a register (arithmetic) instruction
@@ -23,86 +20,93 @@ void arithmeticRegisterExecute(Register_IR *registerIR, Registers regs) {
     // Operand interpreted as an arithmetic type instruction
     uint8_t operand = registerIR->operand.imm6;
 
-    // Get value of register encoded by rm
+    // Set rn value to the 64-bit or 32-bit value of the operand register, determined by sf
     uint64_t rm = getReg(regs, registerIR->rm);
+    rm = registerIR->sf ? rm : (uint32_t) rm;
 
-    // Set src value to the 64-bit or 32-bit value of the source register, determined by sf
-    uint64_t src = registerIR->sf ?
-                   getReg(regs, registerIR->rn) :
-                   (uint32_t) getReg(regs, registerIR->rn);
+    // Set rn value to the 64-bit or 32-bit value of the source register, determined by sf
+    uint64_t rn = getReg(regs, registerIR->rn);
+    rn = registerIR->sf ? rn : (uint32_t) rn;
 
     // Op2 is rm shifted by operand many bits by the encoded shift
-    uint64_t op2 = registerIR->sf ?
-                   bitShift(registerIR->shift, operand, rm, registerIR->sf) :
-                   (uint32_t) bitShift(registerIR->shift, operand, rm, registerIR->sf);
+    uint64_t op2 = bitShift(registerIR->shift, operand, rm, registerIR->sf);
 
     // Initialise result value
     uint64_t res;
 
     PState pState;
+
     switch (registerIR->opc.arithmetic) {
+
+        // Add
         case ADD:
-            res = src + op2;
+            res = rn + op2;
             break;
+
+        // Add (and set flags)
         case ADDS:
-            // Add (and set flags)
-            res = src + op2;
+            res = rn + op2;
             pState.ng = registerIR->sf ? res > INT64_MAX : (uint32_t) res > INT32_MAX;
             pState.zr = res == 0;
-            pState.cr = registerIR->sf ? op2 > UINT64_MAX - src : op2 > UINT32_MAX - src;
-            pState.ov = registerIR->sf ? overflow64(src, op2, res) : overflow32(src, op2, res);
+            pState.cr = registerIR->sf ? op2 > UINT64_MAX - rn : op2 > UINT32_MAX - rn;
+            pState.ov = registerIR->sf ? overflow64(rn, op2, res) : overflow32(rn, op2, res);
             setRegStates(regs, pState);
             break;
+
+        // Subtract
         case SUB:
-            res = src - op2;
+            res = rn - op2;
             break;
+
+        // Subtract (and set flags)
         case SUBS:
-            // Subtract (and set flags)
-            res = src - op2;
+            res = rn - op2;
             pState.ng = registerIR->sf ? res > INT64_MAX : (uint32_t) res > INT32_MAX;
             pState.zr = res == 0;
-            pState.cr = op2 <= src;
-            pState.ov = registerIR->sf ? underflow64(src, op2, res) : underflow32(src, op2, res);
+            pState.cr = op2 <= rn;
+            pState.ov = registerIR->sf ? underflow64(rn, op2, res) : underflow32(rn, op2, res);
             setRegStates(regs, pState);
             break;
+
     }
 
     // Set destination register to the result value, accessed in either 64-bit or 32-bit mode determined by sf
     setReg(regs, registerIR->rd, registerIR->sf, res);
+
 }
 
 /// Determines whether signed overflow has occurred when performing a 64-bit addition
-/// @param src The value of the source register
+/// @param rn The value of the source register
 /// @param op2 The value of the second operand
 /// @param res Result of the addition
 /// @return Whether signed overflow has occurred
-static bool overflow64(int64_t src, int64_t op2, int64_t res) {
-    return (src > 0 && op2 > 0 && res < 0) || (src < 0 && op2 < 0 && res > 0);
+static bool overflow64(int64_t rn, int64_t op2, int64_t res) {
+    return (rn > 0 && op2 > 0 && res < 0) || (rn < 0 && op2 < 0 && res > 0);
 }
 
 /// Determines whether signed overflow has occurred when performing a 32-bit addition
-/// @param src The value of the source register
+/// @param rn The value of the source register
 /// @param op2 The value of the second operand
 /// @param res Result of the addition
 /// @return Whether signed overflow has occurred
-static bool overflow32(int32_t src, int32_t op2, int32_t res) {
-    return (src > 0 && op2 > 0 && res < 0) || (src < 0 && op2 < 0 && res > 0);
+static bool overflow32(int32_t rn, int32_t op2, int32_t res) {
+    return (rn > 0 && op2 > 0 && res < 0) || (rn < 0 && op2 < 0 && res > 0);
 }
 
 /// Determines whether signed underflow has occurred when performing a 64-bit subtraction
-/// @param src The value of the source register
+/// @param rn The value of the source register
 /// @param op2 The value of the second operand
 /// @param res Result of the subtraction
 /// @return Whether signed underflow has occurred
-static bool underflow64(int64_t src, int64_t op2, int64_t res) {
-    return (src > 0 && op2 < 0 && res < 0) || (src < 0 && op2 > 0 && res > 0);
+static bool underflow64(int64_t rn, int64_t op2, int64_t res) {
+    return (rn > 0 && op2 < 0 && res < 0) || (rn < 0 && op2 > 0 && res > 0);
 }
 
 /// Determines whether signed underflow has occurred when performing a 32-bit subtraction
-/// @param src The value of the source register
+/// @param rn The value of the source register
 /// @param op2 The value of the second operand
 /// @param res Result of the subtraction
 /// @return Whether signed underflow has occurred
-static bool underflow32(int32_t src, int32_t op2, int32_t res) {
-    return (src > 0 && op2 < 0 && res < 0) || (src < 0 && op2 > 0 && res > 0);
+static bool underflow32(int32_t rn, int32_t op2, int32_t res) {
+    return (rn > 0 && op2 < 0 && res < 0) || (rn < 0 && op2 > 0 && res > 0);
 }
