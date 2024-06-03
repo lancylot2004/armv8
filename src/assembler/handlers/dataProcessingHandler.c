6cc0d2d831a830ce02182ab5a76d66c8f213255b
Jack Wong <jack.wong23@imperial.ac.uk>
2024-06-03 17:56:47 +0100

    convertAlias fully implemented

--- src/assembler/handlers/dataProcessingHandler.c
+++ src/assembler/handlers/dataProcessingHandler.c
@@ -16,5 +16,61 @@ IR dataProcessingHandler(TokenisedLine *line, AssemblerState *state) {
 /// Converts an assembly form instruction to its alias (if applicable).
 /// @param line The [TokenisedLine] of the instruction.
 /// @return The [TokenisedLine] of the input's alias (or itself, if no alias).
-TokenisedLine convertAlias(TokenisedLine *line) {
+static TokenisedLine convertAlias(TokenisedLine *line) {
+    char *mnemonic = line->mnemonic;
+    char *newMnemonic;
+    int operandCount = 3;
+    char *zr = (line->operands[0][0] == 'x') ? "x31" : "w31";
+    char **operands;
+
+    switch (mnemonic[0]) {
+        case 'c':
+        {
+            if (strcmp(mnemonic, "cmp") == 0) newMnemonic = "subs";
+            else if (strcmp(mnemonic, "cmn") == 0) newMnemonic = "adds";
+            else throwFatal("Instruction mnemonic is invalid!");
+            operands = (char **[][]) {zr, line->operands[0], line->operands[1]};
+            break;
+        }
+        case 'n':
+        {
+            if (strcmp(mnemonic, "neg") == 0) newMnemonic = "sub";
+            else if (strcmp(mnemonic, "negs") == 0) newMnemonic = "subs";
+            else throwFatal("Instruction mnemonic is invalid!");
+            operands = (char **[][]) {line->operands[0], zr, line->operands[1]};
+            break;
+        }
+        case 't':
+        {
+            if (strcmp(mnemonic, "tst") == 0) newMnemonic = "ands";
+            else throwFatal("Instruction mnemonic is invalid!");
+            operands = (char **[][]) {zr, line->operands[0], line->operands[1]};
+            break;
+        }
+        case 'm':
+        {
+            if (strcmp(mnemonic, "mvn") == 0) {
+                newMnemonic = "orn";
+                operands = (char **[][]) {line->operands[0], zr, line->operands[1]};
+            }
+            else if (strcmp(mnemonic, "mov") == 0) {
+                newMnemonic = "orr";
+                operands = (char **[][]) {line->operands[0], zr, line->operands[1]};
+            }
+            else if (strcmp(mnemonic, "mul") == 0) {
+                newMnemonic = "madd";
+                operandCount = 4;
+                operands = (char **[][]) {line->operands[0], line->operands[1], line->operands[2], zr};
+            }
+            else if (strcmp(mnemonic, "mneg") == 0) {
+                newMnemonic = "msub";
+                operandCount = 4;
+                operands = (char **[][]) {line->operands[0], line->operands[1], line->operands[2], zr};
+            }
+            else throwFatal("Instruction mnemonic is invalid!");
+            break;
+        }
+        case default: throwFatal("Instruction mnemonic is invalid!");
+    }
+    return (TokenisedLine) {operandCount, newMnemonic, .operands = operands };
 }
