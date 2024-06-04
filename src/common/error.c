0d36cd701e96ba21dd822c7d2b41be1e5ff25ad6
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-06-04 18:21:03 +0100

    Corrects new error macros.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

--- src/common/error.c
+++ src/common/error.c
@@ -7,18 +7,7 @@
 
 #include "error.h"
 
-/// Assert a condition, pretty-printing and error and exiting if it is not met.
-/// @param condition The condition to assert over. Type: bool
-/// @param message The failure message. Type: char message[]
-/// @throws -1 Exits the program if [condition] is not met.
-#define assertFatal(condition, message) (_assertFatal((condition), (message), __FILE__. __LINE__))
-
-/// Pretty-prints the given error and exits the program.
-/// \param message The failure message. Type: char message[]
-/// @throws -1 Exits the program.
-#define throwFatal(message) (_throwFatal((message), __FILE__, __LINE__))
-
-void _assertFatal(bool condition, char message[], char *file, int line) {
+void assertFatal_(bool condition, char message[], char *file, int line) {
     if (!condition) {
         fprintf(stderr, "%s\n", message);
         fprintf(stderr, "   In file %s, line %d \n\n", file, line);
@@ -27,7 +16,7 @@ void _assertFatal(bool condition, char message[], char *file, int line) {
     }
 }
 
-noreturn void _throwFatal(char message[], char *file, int line) {
+noreturn void throwFatal_(char message[], char *file, int line) {
     fprintf(stderr, "%s\n", message);
     fprintf(stderr, "   In file %s, line %d", file, line);
     perror("    Fatal Error");
