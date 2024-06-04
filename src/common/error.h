dfc0ba6e2e83e04c020a2c80960980468aa58e1a
Jack Wong <jack.wong23@imperial.ac.uk>
2024-06-04 17:44:40 +0100

    Fix assertFatal

--- src/common/error.h
+++ src/common/error.h
@@ -13,11 +13,15 @@
 #include <stdlib.h>
 #include <stdnoreturn.h>
 
+void _assertFatal(bool condition, char message[], char *file, int line);
+
+noreturn void _throwFatal(char message[], char *file, int line);
+
 /// Assert a condition, pretty-printing and error and exiting if it is not met.
 /// @param condition The condition to assert over. Type: bool
 /// @param message The failure message. Type: char message[]
 /// @throws -1 Exits the program if [condition] is not met.
-#define assertFatal(condition, message) (_assertFatal((condition), (message), __FILE__. __LINE__))
+#define assertFatal(condition, message) (_assertFatal((condition), (message), __FILE__, __LINE__))
 
 /// Pretty-prints the given error and exits the program.
 /// \param message The failure message. Type: char message[]
