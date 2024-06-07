f035eef3add16e8c8526ef05d2740980c8825aaf
Lancelot Liu <lancelot.liu23@imperial.ac.uk>
2024-06-07 13:24:07 +0100

    Adds `assertFatalNotNull` and updates documentation.
    Signed-off-by: Lancelot Liu <lancylot2004@proton.me>

--- src/common/error.h
+++ src/common/error.h
@@ -13,19 +13,25 @@
 #include <stdlib.h>
 #include <stdnoreturn.h>
 
-/// Assert a condition, pretty-printing and error and exiting if it is not met.
-/// @param condition The condition to assert over. Type: bool
-/// @param message The failure message. Type: char message[]
-/// @throws -1 Exits the program if [condition] is not met.
-#define assertFatal(condition, message) (assertFatal_(condition, message, __FILE__, __LINE__, __func__))
-
-/// Pretty-prints the given error and exits the program.
-/// \param message The failure message. Type: char message[]
-/// @throws -1 Exits the program.
-#define throwFatal(message) (throwFatal_(message, __FILE__, __LINE__, __func__))
+/// Assert [__CONDITION__], pretty-printing an error and exiting if it is not met.
+/// @param __CONDITION__ The condition to assert over.
+/// @param __MESSAGE__ The failure message.
+#define assertFatal(__CONDITION__, __MESSAGE__) \
+    (assertFatal_(__CONDITION__, __MESSAGE__, __FILE__, __LINE__, __func__))
 
 void assertFatal_(bool condition, char message[], char *file, int line, const char *func);
 
+/// Assert that [__VALUE__] is not null, pretty-printing an error and exiting if it is.
+/// @param __VALUE__ The value to assert not null over.
+/// @param __MESSAGE__ The failure message.
+#define assertFatalNotNull(__VALUE__, __MESSAGE__) \
+    (assertFatal_((__VALUE__) != NULL, __MESSAGE__, __FILE__, __LINE__, __func__))
+
+/// Pretty-prints the given error and exits the program.
+/// @param __MESSAGE__ The failure message.
+/// @warning Will definitely exit the program.
+#define throwFatal(__MESSAGE__) \
+    (throwFatal_(__MESSAGE__, __FILE__, __LINE__, __func__))
 noreturn void throwFatal_(char message[], char *file, int line, const char *func);
 
 #endif // COMMON_ERROR_H
