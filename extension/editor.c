81aa0d028835946c0f150c9df448588bcdb4bdae
Billy Highley <billy.highley23@imperial.ac.uk>
2024-06-12 18:55:06 +0100

    Create extension skeleton

---
+++ extension/editor.c
@@ -0,0 +1,12 @@
+///
+/// editor.c
+/// Instantiates an editor (terminal-based) interface.
+///
+/// Created by Billy Highley on 12/06/24.
+///
+
+#include "editor.h"
+
+int main(int argc, char **argv) {
+    return 0;
+}
---
+++ extension/editor.h
@@ -0,0 +1,11 @@
+///
+/// editor.h
+/// Instantiates an editor (terminal-based) interface.
+///
+/// Created by Billy Highley on 12/06/24.
+///
+
+#ifndef EXTENSION_EDITOR_H
+#define EXTENSION_EDITOR_H
+
+#endif //EXTENSION_EDITOR_H
