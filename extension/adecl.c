d37095aa935209c66975b622463c6f37f812e7e8
Jack Wong <jack.wong23@imperial.ac.uk>
2024-06-19 16:14:00 +0100

    created adecl file and function declaration

---
+++ extension/adecl.c
@@ -0,0 +1,18 @@
+///
+/// adecl.c
+/// Translates an assembly instructoin into a human readable description
+///
+/// Created by Jack Wong on 19/06/24.
+///
+
+#include "adecl.h"
+
+/// Translates [irObject] to its human readable description.
+/// @param irObject The instruction to interpret.
+/// @returns The human readable description.
+char *adecl(IR *irObject) {
+
+    switch (irObject->type) {
+
+    }
+}
